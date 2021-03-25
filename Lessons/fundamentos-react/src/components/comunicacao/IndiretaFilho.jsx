e9f28fae447e5d7224d2cd3d2a1bb7db51865646
DayanMonteiro <dmsmamb@gmail.com>
2021-03-25 19:43:27 -0300

    useState

--- Lessons/fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
+++ Lessons/fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 export default props => {
     const cb = props.quandoClicar
+    const min = 50
+    const max = 70
+    const gerarIdade = () => parseInt(Math.random() * (max - min)) + min
+    const gerarNerd = () => Math.random() > 0.5
     return (
         <div>
             <div> 
                 Filho
             </div>
-            <button onClick= {_ => cb('Daniel', 59, false)}>
+            <button onClick= {_ => cb('Daniel', gerarIdade(), gerarNerd())}>
                 Fornecer Informações
             </button>
         </div>
