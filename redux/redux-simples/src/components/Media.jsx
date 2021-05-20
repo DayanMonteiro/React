0974d9c1ccff852e729ed8c25e74505a78edd17e
DayanMonteiro <dmsmamb@gmail.com>
2021-05-19 22:56:24 -0300

    conectando o componente ao estado da aplicação

--- redux/redux-simples/src/components/Media.jsx
+++ redux/redux-simples/src/components/Media.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
+// responsável por conectar o componente ao estado da aplicação
+import { connect } from 'react-redux'
+
 import Card from './Card'
 
-export default props => {
+
+const Media = props => {
+    const { min, max } = props
     return (
         <Card title="Média dos Números" green>
             <div>
                 <span> 
                     <span>Resultado: </span>
-                    <strong>{10}</strong>
+                    <strong>{(max + min) / 2}</strong>
                 </span>
             </div>
         </Card>
     )
 }
+
+function mapStateToProps(state) {
+    return {
+        min: state.numeros.min,
+        max: state.numeros.max
+    }
+}
+
+export default connect(mapStateToProps) (Media)
